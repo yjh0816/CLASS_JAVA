b71d1e3c094b14bd47e1e262ec1a08813913e1de
yjh0816 <2543kt@naver.com>
2023-03-02 16:43:44 +0900

    feat: Implement input test

---
+++ day1/src/day1/InputTest.java
@@ -0,0 +1,15 @@
+package day1;
+
+import java.util.Scanner;
+
+public class InputTest {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int a = input.nextInt();
+		int c = input.nextInt();
+		String b = input.next();
+		System.out.println(a+b);
+	}
+
+}
