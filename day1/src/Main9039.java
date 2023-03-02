3ffbafe0d32156f146af1e4d937f84e143ab2cf4
yjh0816 <2543kt@naver.com>
2023-03-02 16:19:38 +0900

    feat: Solve JUNGOL 9030 problem

---
+++ day1/src/Main9039.java
@@ -0,0 +1,13 @@
+
+public class Main9039 {
+
+	public static void main(String[] args) {
+		char ar = 'A';
+		for (int i=0; i<26; i++) { 
+//			System.out.print((char)(ar+i));
+			System.out.print(ar++);
+		}
+
+	}
+
+}
