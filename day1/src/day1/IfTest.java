e28b700a2d5f619abc03f30917c2a600b21000a9
yjh0816 <2543kt@naver.com>
2023-03-02 14:54:09 +0900

    feat: Implement statement test

---
+++ day1/src/day1/IfTest.java
@@ -0,0 +1,44 @@
+package day1;
+
+public class IfTest {
+
+	public static void main(String[] args) {
+		int score = 77;
+		// if 1
+		if (score > 70) {
+			System.out.println("Pass~~~");
+		}
+		// if 2
+		if (score > 80) {
+			System.out.println("Pass");
+		} else {
+			System.out.println("Non-Pass:(");
+		}
+		// if 3
+		if (score >= 90) {
+			System.out.println("grade A");
+		} else if (score >= 80) {
+			System.out.println("grade B");
+		} else if (score >= 70) {
+			System.out.println("grade C");
+		} else {
+			System.out.println("grade F");
+		}
+		// switch
+		switch (score/10) {
+			case 10:
+			case 9:
+				System.out.println("grade A");
+				break;
+			case 8:
+				System.out.println("grade B");
+				break;
+			case 7:
+				System.out.println("grade C");
+				break;
+			default:
+				System.out.println("grade F");
+		}
+	} // end main()
+
+} // end class
