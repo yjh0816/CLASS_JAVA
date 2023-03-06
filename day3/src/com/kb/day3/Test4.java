032a076781e5d97eb640f072866e9e0c887a48d6
yjh0816 <2543kt@naver.com>
2023-03-06 13:57:21 +0900

    feat: Implement class array test

---
+++ day3/src/com/kb/day3/Test4.java
@@ -0,0 +1,14 @@
+package com.kb.day3;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		MyDate mydate = new MyDate(3,4);
+		System.out.println(mydate);
+		
+		MyDate mydate2 = new MyDate(3,23);
+		System.out.println(mydate2);
+		
+	}
+
+}
---
+++ day3/src/com/kb/day3/Test5.java
@@ -0,0 +1,28 @@
+package com.kb.day3;
+
+public class Test5 {
+
+	public static void main(String[] args) {
+		int[] su = new int[5];
+//		for(int s:su) {
+//			System.out.print(s);
+//		}
+		// 00000
+		su[1] = 30;
+		
+		MyDate[] mydate = new MyDate[5];
+//		for(MyDate md : mydate) {
+//			System.out.print(md);
+//		}
+		// nullnullnullnullnull
+		mydate[0] = new MyDate(3,23);
+		mydate[1] = new MyDate(4,21);
+		mydate[2] = new MyDate(5,5);
+		mydate[3] = new MyDate(5,15);
+		mydate[4] = new MyDate(5,29);
+		for(MyDate md : mydate) {
+			System.out.println(md);
+		}
+	}
+
+}
