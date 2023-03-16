6c5061c0e1cec3ca4010dc9a90fa7bf9b418756d
yjh0816 <2543kt@naver.com>
2023-03-16 13:41:54 +0900

    feat: Solve BOJ 11047 동전0

---
+++ Algorithm/src/day6/BOJ11047_동전0.java
@@ -0,0 +1,27 @@
+package day6;
+
+import java.util.Scanner;
+
+public class BOJ11047_동전0 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int count = 0;
+		int temp;
+		for (int i = N - 1; i >= 0; i--) {
+			temp = (int)(K / arr[i]);
+			if(temp > 0) {
+				count += temp;
+				K -= temp*arr[i];
+			}
+		}
+		System.out.println(count);
+	}
+
+}
