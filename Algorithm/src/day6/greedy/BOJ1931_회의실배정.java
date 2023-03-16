b3ffc47542f5cbdc7ec820ad09ae4fa180f8cd1c
yjh0816 <2543kt@naver.com>
2023-03-16 16:42:20 +0900

    feat: Solve BOJ1931 회의실배정

--- Algorithm/src/day6/greedy/BOJ1931_회의실배정.java
+++ Algorithm/src/day6/greedy/BOJ1931_회의실배정.java
@@ -1,10 +1,57 @@
 package day6.greedy;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+class Meeting implements Comparable<Meeting> {
+	int start;
+	int end;
+	
+	public Meeting(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
+	@Override
+	public int compareTo(Meeting o) {
+		return this.end != o.end ? this.end - o.end : this.start - o.start;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Meeting [start=");
+		builder.append(start);
+		builder.append(", end=");
+		builder.append(end);
+		builder.append("]");
+		return builder.toString();
+	}
+}
 public class BOJ1931_회의실배정 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		ArrayList<Meeting> mt = new ArrayList();
+		ArrayList<Meeting> result = new ArrayList();
+		int count = 0;
+		for (int i = 0; i < N; i++) {
+			int start = sc.nextInt();
+			int end = sc.nextInt();
+			mt.add(new Meeting(start, end));
+		}
+		
+		Collections.sort(mt);
+//		System.out.println(mt.toString());
+		result.add(mt.get(0));
+		count++;
+		for (int i = 1; i < N; i++) {
+			if (result.get(count - 1).end <= mt.get(i).start) {
+				result.add(mt.get(i));
+				count++;
+			}
+		}
+		System.out.println(count);
 	}
 
 }
