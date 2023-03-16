8ad576b6223e6b73b129fee670ae8ad04e364160
yjh0816 <2543kt@naver.com>
2023-03-16 15:38:57 +0900

    fix: Change package name day6 to day6.greedy

---
+++ Algorithm/src/day6/greedy/BOJ1931_회의실배정.java
@@ -0,0 +1,10 @@
+package day6.greedy;
+
+public class BOJ1931_회의실배정 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Algorithm/src/day6/greedy/BOJ2839_설탕배달.java
+++ Algorithm/src/day6/greedy/BOJ2839_설탕배달.java
@@ -1,4 +1,4 @@
-package day6;
+package day6.greedy;
 
 import java.util.Scanner;
 
